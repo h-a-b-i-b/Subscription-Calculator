57708095e7f84a86afd4af2ba5ebc9d6ea7a375e
Md Habibulla <habibullamd197@gmail.com>
2023-10-23 20:44:47 +0530

    User Interface Enhancements:
    Added a new container for the entire calculator to improve the overall layout.
    Styled the calculator with a border, hover effects, and rounded corners to make it visually appealing.
    Included icons with tooltips to provide information to users.
    Input Container:
    
    Grouped input fields and checkboxes in a single container for better organization.
    Table Container:
    
    Created a separate container for the comparison table.
    Add Plan to Compare:
    
    Improved the "Add Plan to Compare" button to be visually appealing with hover effects.
    Added a "Clear Table" button for user convenience.
    Include GST Feature:
    
    Integrated the "Include GST" checkbox into the calculator.
    Modified the code to apply 18% GST when calculating costs if the checkbox is selected.
    Calculation Adjustments:
    
    Updated the calculation to provide cost per day, month, and year. Removed cost per hour.
    Highlighted the best plan with a light green background in the table.
    Table Data Format:
    
    Formatted the table data to display rate, periods, cost per day, cost per month, and cost per year.
    GitHub Documentation:
    
    Updated the documentation to reflect the changes and improvements made to the calculator.
    Enhanced the UI and provided detailed instructions on how to use the calculator.
    CSS and JS Updates:
    
    Included updated CSS and JavaScript code to match the improved UI and functionality.

--- script.js
+++ script.js
@@ -20,30 +20,71 @@ function updateComparisonTable(newPlan) {
     const resultContainer = document.getElementById('planComparison');
     const daysDifference = newPlan.validityPeriodToCompare;
     let costPerDay = newPlan.rechargeAmountToCompare / newPlan.validityPeriodToCompare;
-    let costPerYear = newPlan.rechargeAmountToCompare * (365 / newPlan.validityPeriodToCompare);
-    let costPerHour = costPerYear / (365 * 24);
+    let costPerYear = (newPlan.rechargeAmountToCompare * 365) / newPlan.validityPeriodToCompare;
+    let costPerMonth = costPerYear / 12;
 
     if (newPlan.includeGST) {
         costPerDay *= 1.18; // Apply 18% GST
         costPerYear *= 1.18;
-        costPerHour *= 1.18;
+        costPerMonth *= 1.18;
     }
 
     costPerDay = costPerDay.toFixed(2);
     costPerYear = costPerYear.toFixed(2);
-    costPerHour = costPerHour.toFixed(2);
+    costPerMonth = costPerMonth.toFixed(2);
 
     const planRow = document.createElement('tr');
     planRow.innerHTML = `
         <td>${newPlan.rechargeAmountToCompare} Rupees</td>
         <td>${daysDifference} Days</td>
-        <td>₹${costPerDay}</td>
-        <td>₹${costPerYear}</td>
-        <td>₹${costPerHour}</td>
+        <td>${costPerDay} Rupees</td>
+        <td>${costPerMonth} Months</td>
+        <td>${costPerYear} Years</td>
     `;
 
     resultContainer.appendChild(planRow);
+
+    highlightBestPlan();
 }
+
+function highlightBestPlan() {
+    const rows = document.querySelectorAll('#planComparison tr');
+    if (rows.length > 1) {
+        const bestPlanIndex = findBestPlanIndex();
+        rows.forEach((row, index) => {
+            if (index === bestPlanIndex) {
+                row.classList.add('best-plan');
+            } else {
+                row.classList.remove('best-plan');
+            }
+        });
+    }
+}
+
+function findBestPlanIndex() {
+    let bestPlanIndex = 0;
+    let bestCostPerMonth = parseFloat(
+        document
+            .querySelectorAll('#planComparison tr td:nth-child(4)')[0]
+            .textContent.replace(/[^\d.]/g, '')
+    );
+
+    for (let i = 1; i < planDataToCompare.length; i++) {
+        const costPerMonth = parseFloat(
+            document
+                .querySelectorAll(`#planComparison tr:nth-child(${i + 1}) td:nth-child(4)`)[0]
+                .textContent.replace(/[^\d.]/g, '')
+        );
+
+        if (costPerMonth < bestCostPerMonth) {
+            bestCostPerMonth = costPerMonth;
+            bestPlanIndex = i;
+        }
+    }
+
+    return bestPlanIndex;
+}
+
 function clearComparisonTable() {
     const resultContainer = document.getElementById('planComparison');
     resultContainer.innerHTML = '';
